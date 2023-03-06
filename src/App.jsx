bd0992d63172936231530a80f32101e9f0db562c
quyet <leminhquyet1204@gmail.com>
2023-03-06 17:10:24 +0700

    add loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react";
+
 import "./App.css";
 import Header from "./components/header/Header";
 
@@ -5,16 +7,34 @@ import Home from "./components/home/Home";
 import Portfolio from "./components/portfolio/Portfolio";
 import Skills from "./components/skills/Skills";
 import Contact from "./components/contact/Contact";
+import Loading from "./pages/Loading";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const waitTimer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => {
+      clearTimeout(waitTimer);
+    };
+  }, []);
   return (
     <main>
-      <Header />
-      <Home />
-      <Skills />
-      <Portfolio />
-      <Contact />
-      {/* <Footer /> */}
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <Header />
+          <Home />
+          <Skills />
+          <Portfolio />
+          <Contact />
+        </>
+      )}
     </main>
   );
 }
